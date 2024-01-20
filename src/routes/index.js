6889d8a8904a35e6994f65c3e6070d1240f0050a
Christian Nascimento <christian.caique@hotmai.com>
2024-01-20 12:10:41 -0300

    Cria controller, model e router de Autor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import livros from "./livrosRouters.js";
+import autor from "./autoresRouters.js"
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send("Curso de Node.js");
   });
 
-  app.use(express.json(), livros);
+  app.use(express.json(), livros, autor);
 
 };
 export default routes;
